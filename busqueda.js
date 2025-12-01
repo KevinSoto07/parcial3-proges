6f3574a54d76ceb57d93bc89acd49d6459c8f94f
Fabrizzio-7 <fabrizzio.ez77@gmail.com>
2025-11-30 23:10:29 -0600

    busqueda js

--- busqueda.js
+++ busqueda.js
@@ -1,3 +1,30 @@
-function buscarNombre(callback){
+import estudiantes from "./listado.js";  // tu lista de estudiantes
+
+export function buscarNombre(callback) {
+
+    const rl = readline.createInterface({
+        input:process.stdin,
+        output: process.stdout
+    })
     
-}
+
+        rl.question("Ingrese el nombre del estudiante a buscar: ", (nombre) => {
+            
+            const encontrado = estudiantes.find(est => 
+                est.nombre.toLowerCase() === nombre.toLowerCase()
+            );
+
+            if (encontrado) {
+                console.log("\n Estudiante encontrado:");
+                console.log(`Nombre: ${encontrado.nombre}`);
+                console.log(`Edad: ${encontrado.edad}`);
+                console.log(`Notas: ${encontrado.notas.join(", ")}`);
+                console.log(`Promedio: ${encontrado.promedio}\n`);
+            } else {
+                console.log("\n No se encontró un estudiante con ese nombre.\n");
+            }
+
+            rl.close();
+            callback();
+        });
+    };
